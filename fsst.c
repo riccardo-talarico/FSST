99cac6aa3ef4bd1abdce7c41896759cc964ab565
riccardo-talarico <riccardotalarico363@gmail.com>
2026-01-16 12:23:21 +0100

    Add compressCount

--- fsst.c
+++ fsst.c
@@ -56,19 +56,19 @@ byte findLongestSymbol(symbolTable *st, byte *text){
 
 
 void encode(symbolTable *st, byte **in, byte **out){
-	byte pos = findLongestSymbol(st, *in);
+	byte code = findLongestSymbol(st, *in);
 #if DEBUG
-	printf("Position found: %u\n",pos);
+	printf("Code found: %u\n",code);
 #endif
-	if(pos == 255){
+	if(code == 255){
 		// Adding escape sequence
 		*((*out)++) = 255;	
 		// Copying the byte
 		*((*out)++) = *((*in)++);
 	}
 	else{
-		*((*out)++) = pos;
-		*in += st->entry[pos].len;
+		*((*out)++) = code;
+		*in += st->entry[code].len;
 	}
 }	
 
@@ -88,6 +88,45 @@ void decode(symbolTable *st, byte **in, byte **out){
 }
 
 
+void compressCount(symbolTable *st, uint32_t *count1, uint32_t count2[][512], char *text, size_t text_len){
+	size_t pos = 0;
+	byte *t = (byte*)text;
+	byte code = findLongestSymbol(st,t);
+
+	size_t symbolLen = (code==255)? 1 : st->entry[code].len; 
+	if(code==255) code=t[pos];
+	else code+=255;
+	count1[code]++;
+#if DEBUG
+	printf("Symbol len %lu\n", symbolLen);
+#endif
+	byte prev = code;
+	while((pos+=symbolLen)<text_len){
+		code = findLongestSymbol(st, t+pos);
+#if DEBUG
+		printf("Code found: %u, current prev: %u, current pos: %lu\n", code, prev, pos);
+#endif
+		if(code == 255){
+			byte next = t[pos];
+#if DEBUG
+			printf("Byte: %u\n", next);
+#endif
+			count1[next]++;
+			count2[prev][next]++;
+			prev=next;
+		}
+		else{
+			// Symbol frequency count is stored from 255 on
+			code += 255;
+			// Count frequencies of symbols
+			count1[code]++;
+			// Count frequencies of concat(prev,code)
+			count2[prev][code]++;
+			prev=code;
+		}	
+		symbolLen = (code==255)? 1 : st->entry[code].len; 
+	}
+}
 
 /*
 symbolTable *buildSymbolTableFromText(char *text){
@@ -102,10 +141,12 @@ symbolTable *buildSymbolTableFromText(char *text){
 
 int main(void){
 	symbolTable *st = stInit();
-	char *text = "Hello World";
+	// Repetition of tum, len=13
+	char *text = "tumcwitumvldb";
 	
 	byte *p = (byte*)text;
-	byte *outbuf = malloc(sizeof(*text)*2 + 1);
+	byte *outbuf = malloc(strlen(text)*2 + 1);
+	printf("Size of outbuf:%lu\n",strlen(text)*2+1);
 	byte *outp = outbuf;
 	
 	while(p[0]!=0){
@@ -127,11 +168,18 @@ int main(void){
 		else printf("Escape-");
 	}
 	printf("\n");
+	
+#if DEBUG
+	printf("Initiating decoding\n");
+#endif
 	byte *decoded = malloc(sizeof(*text)+1);
 	p = decoded;
 	byte *encoded = outbuf;
 	while(outp != encoded){
-	   	decode(st, &encoded, &p); 
+#if DEBUG
+		printf("Pointer %p\n", p);
+#endif
+		decode(st, &encoded, &p); 
 	}
 	printf("Successfully Decoded\n");
 	
@@ -144,7 +192,34 @@ int main(void){
 		else printf("Unprintable, value %u\n",c);
 	}
 	printf("\n");
+#if DEBUG
+	printf("Start compressCount\n");
+#endif
+	uint32_t count1[512]={0};
+	uint32_t count2[512][512]={0};
+	size_t text_len = strlen(text);
+	compressCount(st, count1, count2, text, text_len);
 
+	for(unsigned int i = 0; i < 512; i++){
+		uint32_t freq = count1[i];
+		if(freq > 0){
+			byte c = (i>=255)? (byte)(i-255) : (byte)i;
+			if(isprint(c)) printf("Char %c has %u occurrencies\n",c,freq);
+			else printf("Unprintable value %u has %u occurrencies\n",c,freq);
+		}
+	}
+
+	for(unsigned int i = 0; i < 512; i++){
+		for(unsigned int j = 0; j < 512; j++){
+			uint32_t freq = count2[i][j];
+			if(freq>0){
+				byte c1 = (i>=255)? (byte)(i-255) : (byte)i;
+				byte c2 = (j>=255)? (byte)(j-255) : (byte)j;
+				if(isprint(c1) && isprint(c2)) printf("Concat (%c, %c) has frequency: %u\n", c1,c2,freq);
+				else printf("Concat byte (%u,%u) has frequency: %u\n", c1,c2,freq);
+			}
+		}
+	}
 
 	free(st);
 	free(decoded);
