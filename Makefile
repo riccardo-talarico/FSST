387699a2088b733230e39efff8117a97898d5cb8
riccardo-talarico <riccardotalarico363@gmail.com>
2026-02-02 16:45:21 +0100

    Refactored into header file

--- Makefile
+++ Makefile
@@ -3,17 +3,20 @@ CC = gcc
 CFLAGS = -Wall -W -O2
 
 
-TARGET = fsst
+TARGET = test_fsst
 
-OBJ = fsst.o heap.o
+OBJ = test_fsst.o hashFsst.o heap.o
 
 all: $(TARGET)
 
 $(TARGET): $(OBJ)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJ)
 
-fsst.o: fsst.c heap.h
-	$(CC) $(CFLAGS) -c fsst.c
+test.o: test_fsst.c hashFsst.h 
+	$(CC) $(CFLAGS) -c test_fsst.c
+
+hashFsst.o: hashFsst.c hashFsst.h heap.h
+	$(CC) $(CFLAGS) -c hashFsst.c
 
 heap.o: heap.c heap.h
 	$(CC) $(CFLAGS) -c heap.c
--- hashFsst.c
+++ hashFsst.c
@@ -5,57 +5,7 @@
 #include <ctype.h>
 #include <unistd.h>
 #include "heap.h"
-
-#define DEBUG 1
-#define ESCAPE_CODE 255
-#ifdef DEBUG
-    #define debug_print(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
-	#define debug_print_symbol(s, slen) do{\
-		for(int _debug_i = 0; _debug_i < (slen); _debug_i++) {\
-		   	if(isprint(s[_debug_i])) fprintf(stderr,"%c",(char)s[_debug_i]); \
-			else fprintf(stderr,"%u",s[_debug_i]); \
-		} \
-	}while(0)
-#else
-    #define debug_print(fmt, ...) do {} while (0)
-	#define debug_print_symbol(s, slen) do {} while(0)
-#endif
-
-#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-    #define IS_LITTLE_ENDIAN 0
-    // Mask the most significant 3 bytes (0, 1, 2 in memory)
-    #define THREE_BYTE(x) ((x) & 0xFFFFFF0000000000ULL)
-#else
-    #define IS_LITTLE_ENDIAN 1
-    // Mask the least significant 3 bytes (0, 1, 2 in memory)
-    #define THREE_BYTE(x) ((x) & 0x0000000000FFFFFFULL)
-#endif
-
-typedef uint8_t byte;
-
-typedef struct symbolEntry{	
-	uint16_t code; //bits [0..8]=code, [12..15]=len, unused=511 because it can be casted to 255 easily
-	// can range from 1 to 8 byte
-	union{
-		byte symbol[8];
-		uint64_t num;
-	};
-	byte ignoredBits;
-}symbolEntry;
-
-
-/* Symbol Table data structure. It contains an array of 255 symbols plus a special entry for the escape byte
- * The symbols are stored in lexicographical order but when one string prefixes the other, the longer is first.
- * The firsIdx array stores for every byte the code of the longest symbol that start with that byte
- */
-#define HASH_TABLE_SIZE 4096 //fits in L1 cache
-#define SHORT_CODES_SIZE 256*256
-typedef struct symbolTable{
-	symbolEntry hashTable[HASH_TABLE_SIZE];
-	symbolEntry entry[255];
-	uint16_t shortCodes[SHORT_CODES_SIZE];
-	byte nSymbols;
-}symbolTable;
+#include "hashFsst.h"
 
 symbolTable *stInit(void){
 	symbolTable *st = malloc(sizeof(*st));
@@ -70,13 +20,11 @@ symbolTable *stInit(void){
 	return st;
 }
 
-#define MAGIC_HASH 2971215073
 uint64_t hash(uint64_t x){
 	x = THREE_BYTE(x);
 	return ((x*MAGIC_HASH)^(x>>15)) & (HASH_TABLE_SIZE-1);
 }
 
-#define SYMBOL_LEN(c) (((c) >> 12) & 0x0F)
 void insertSymbol(symbolTable *st, symbolEntry e){
 	st->hashTable[hash(e.num)] = e;
 
@@ -123,7 +71,6 @@ void encodeScalar(uint8_t *cur, uint8_t *out, symbolTable *st){
 	cur += SYMBOL_LEN(code); // symbol length is in bits [12..15] of code 
 }
 
-#define ESCAPE_CODE 255
 void compressCount(const symbolTable *st, uint32_t *count1, uint32_t count2[][512], const char *text, 
 		const size_t text_len){
 	size_t pos = 0;
@@ -298,18 +245,3 @@ symbolTable *buildSymbolTableFromText(char *text){
 	return st;
 }
 
-
-
-int main(void){
-	
-	char *text = "tumcwitumvldb";
-
-	symbolTable *st = buildSymbolTableFromText(text);
-	for(int i = 0; i < st->nSymbols; i++){
-		if(st->entry[i].code != 0){
-			printf("Symbol: %s in entry %d\n", st->entry[i].symbol,i);
-		}
-	}
-	free(st);
-	return 0;
-}
